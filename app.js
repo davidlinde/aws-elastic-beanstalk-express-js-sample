00628ca063c827eabcd2d1b94ef87ad019654aa6
David Linde <linded@bellsouth.net>
2026-08-28 11:50:31 -0400

    explicitly set listen host address.

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port);
+app.listen(port, '0.0.0.0');
 console.log(`App running on http://localhost:${port}`);
